89753903d5eaf33781a4e30886e5d106a7dcb66b
Kamal Thennakoon <thennakoon.kamal@gmail.com>
2020-08-21 08:35:31 +0530

    building routes

--- controllers/building-controller.js
+++ controllers/building-controller.js
@@ -1,6 +1,22 @@
 const Building = require("../models/building");
 const HttpError = require("../models/http-error");
 
+const getBuildingById = async (req, res, next) => {
+  const bid = req.params.bid;
+  let building;
+  try {
+      building = await Building.findById(bid);
+  } catch (err) {
+      const error = new HttpError('Something went wrong on DB side', 500);
+      return next(error);
+  }
+
+  if (!building) {
+      return next(new HttpError('No building found for the given id!!', 404));
+  }
+  res.json({ building: building.toObject({ getters: true }) });
+};
+
 
 
 const getAllBuildings =  async(req, res) => {
@@ -15,7 +31,7 @@ const getAllBuildings =  async(req, res) => {
     const error=new HttpError('No Buildings found',404);  
     return next(error);
   }
-  res.json({buildings:buildings.toObject({getters:true})});
+  res.json({buildings:buildings.map((b)=>(b.toObject({getters:true})))});
   
 };
 
@@ -38,7 +54,68 @@ const addBuilding = async (req, res, next) => {
     return next(error);
   }
 
-  res.status(201).json({Location:newBuilding.toObject({getters:true})});
+  res.status(201).json({msg:'Building has been added successfully!'});
+};
+
+const updateBuilding = async (req, res, next) => {
+  
+  const buildingId = req.params.bid;
+  const { buildingName,lecHallCapacity,labCapacity,description } = req.body;
+  let selectedBuilding;
+  try {
+      selectedBuilding = await Building.findById(buildingId);
+  } catch (err) {
+      const error = new HttpError(
+          'something went wrong on db side, when finding the Building id',
+          500
+      );
+      return next(error);
+  }
+  selectedBuilding.buildingName = buildingName;
+  selectedBuilding.lecHallCapacity = lecHallCapacity;
+  selectedBuilding.labCapacity = labCapacity;
+  selectedBuilding.description = description;
+
+  try {
+      await selectedBuilding.save();
+  } catch (err) {
+      const error = new HttpError('something went wrong on db side', 500);
+      return next(error);
+  }
+
+  res.status(200).json({
+      building: selectedBuilding.toObject({ getters: true }),msg:'Building has updated successfully!!'
+  });
+};
+
+const deleteBuilding = async (req, res, next) => {
+  const buildingId = req.params.bid;
+  let selectedBuilding;
+  try {
+      selectedBuilding = await Building.findById(buildingId);
+  } catch (err) {
+      const error = new HttpError(
+          'something went wrong on db side, when finding the given building id'
+      );
+      return next(error);
+  }
+
+  if (!selectedBuilding) {
+      return next(
+          new HttpError('there is no record for the given building id', 404)
+      );
+  }
+
+  try {
+      await selectedBuilding.remove();
+  } catch (err) {
+      const error = new HttpError(
+          'couldnt delete the record, something went wrong on db side'
+      );
+      return next(error);
+  }
+
+  res.status(200).json({ building: 'Building has been deleted' });
 };
 
 
@@ -46,3 +123,6 @@ const addBuilding = async (req, res, next) => {
 
 exports.getAllBuildings=getAllBuildings;
 exports.addBuilding=addBuilding;
+exports.updateBuilding=updateBuilding;
+exports.deleteBuilding=deleteBuilding;
+exports.getBuildingById=getBuildingById;
--- routes/building-routes.js
+++ routes/building-routes.js
@@ -6,9 +6,11 @@ const { AddBuildingValidator } = require('../validators/building-validator');
 const { runValidation } = require('../validators/validator');
 
 
+router.get('/:bid',BuildingController.getBuildingById);
 router.get('/',BuildingController.getAllBuildings);
 router.post('/',AddBuildingValidator,runValidation,BuildingController.addBuilding);
-
+router.delete('/:bid',BuildingController.deleteBuilding);
+router.patch('/:bid',BuildingController.updateBuilding);
 router.get('/test',((req,res)=>{
     res.status(200).json({
         error:"we are up!!"
