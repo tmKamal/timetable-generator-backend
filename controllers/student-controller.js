fe191faf559a3f0d899838d9ce4cece343d96360
Ashen Kavinda <49136460+Ashenk97@users.noreply.github.com>
2020-09-05 09:51:12 +0530

    All Crud in tag done

--- controllers/student-controller.js
+++ controllers/student-controller.js
@@ -1,6 +1,22 @@
 const Student = require("../models/student");
 const HttpError = require("../models/http-error");
 
+const getStudentById = async (req, res, next) => {
+  const sid = req.params.sid;
+  let student;
+  try {
+    student = await Student.findById(sid);
+  } catch (err) {
+    const error = new HttpError("Something went wrong on DB side", 500);
+    return next(error);
+  }
+
+  if (!student) {
+    return next(new HttpError("No student found for the given id!!", 404));
+  }
+  res.json({ student: student.toObject({ getters: true }) });
+};
+
 const getAllStudents = async (req, res) => {
   let students;
   try {
@@ -13,7 +29,7 @@ const getAllStudents = async (req, res) => {
     const error = new HttpError("No Students found", 404);
     return next(error);
   }
-  res.json({ students: students.toObject({ getters: true }) });
+  res.json({ students: students.map((b) => b.toObject({ getters: true })) });
 };
 
 const addStudent = async (req, res, next) => {
@@ -37,8 +53,72 @@ const addStudent = async (req, res, next) => {
     return next(error);
   }
 
-  res.status(201).json({ Location: newStudent.toObject({ getters: true }) });
+  res.status(201).json({ msg: "Student has been added successfully!" });
+};
+
+const updateStudent = async (req, res, next) => {
+  const studentId = req.params.sid;
+  const { academicYearSem, programme, groupNumber, subGroupNumber } = req.body;
+  let selectedStudent;
+  try {
+    selectedStudent = await Student.findById(studentId);
+  } catch (err) {
+    const error = new HttpError(
+      "something went wrong on db side, when finding the Stdudent id",
+      500
+    );
+    return next(error);
+  }
+  selectedStudent.academicYearSem = academicYearSem;
+  selectedStudent.programme = programme;
+  selectedStudent.groupNumber = groupNumber;
+  selectedStudent.subGroupNumber = subGroupNumber;
+
+  try {
+    await selectedStudent.save();
+  } catch (err) {
+    const error = new HttpError("something went wrong on db side", 500);
+    return next(error);
+  }
+
+  res.status(200).json({
+    student: selectedStudent.toObject({ getters: true }),
+    msg: "Student has updated successfully!!",
+  });
+};
+
+const deleteStudent = async (req, res, next) => {
+  const studentId = req.params.sid;
+  let selectedStudent;
+  try {
+    selectedStudent = await Student.findById(studentId);
+  } catch (err) {
+    const error = new HttpError(
+      "something went wrong on db side, when finding the given student id"
+    );
+    return next(error);
+  }
+
+  if (!selectedStudent) {
+    return next(
+      new HttpError("there is no record for the given student id", 404)
+    );
+  }
+
+  try {
+    await selectedStudent.remove();
+  } catch (err) {
+    const error = new HttpError(
+      "couldnt delete the record, something went wrong on db side"
+    );
+    return next(error);
+  }
+
+  res.status(200).json({ student: "Student has been deleted" });
 };
 
 exports.getAllStudents = getAllStudents;
 exports.addStudent = addStudent;
+exports.updateStudent = updateStudent;
+exports.deleteStudent = deleteStudent;
+exports.getStudentById = getStudentById;
--- routes/student-routes.js
+++ routes/student-routes.js
@@ -5,6 +5,7 @@ const StudentController = require("../controllers/student-controller");
 const { AddStudentValidator } = require("../validators/student-validator");
 const { runValidation } = require("../validators/validator");
 
+router.get("/:sid", StudentController.getStudentById);
 router.get("/", StudentController.getAllStudents);
 router.post(
   "/",
@@ -12,7 +13,8 @@ router.post(
   runValidation,
   StudentController.addStudent
 );
-
+router.delete("/:sid", StudentController.deleteStudent);
+router.patch("/:sid", StudentController.updateStudent);
 router.get("/test", (req, res) => {
   res.status(200).json({
     error: "we are up!!",
